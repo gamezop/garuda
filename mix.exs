d873b8092702b3eabe12f2088f40a978ae29a57c
anandu <pavanan.anandu63@gmail.com>
2021-01-30 16:18:03 +0530

    mix.exs changes

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Garuda.MixProject do
   def project do
     [
       app: :garuda,
-      version: "0.2.0-rc.2",
+      version: "0.2.0",
       elixir: "~> 1.10",
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
