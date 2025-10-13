9ad653496a5912b56ee3b332927969b5b7be84ee
ghostdsb <dibyanshu.bhoi@gmail.com>
2025-10-13 11:49:06 +0530

    fix: 🐛 remove player from matchmaker when disconneting lobby

--- lib/matchmaker/matchmaker_channel.ex
+++ lib/matchmaker/matchmaker_channel.ex
@@ -5,11 +5,18 @@ defmodule Garuda.MatchMakerChannel do
 
   def join("garuda_matchmaker:lobby", match_details, socket) do
     match_resp = Matcher.join_or_create(match_details)
+    socket = assign(socket, :mm_player_id, match_details["player_id"])
 
     if Map.has_key?(match_resp, "match_id") do
+      socket = assign(socket, :mm_match_id, match_resp["match_id"])
       {:ok, match_resp, socket}
     else
       {:error, match_resp}
     end
+  end
+
+  def terminate(_reason, socket) do
+    Matcher.remove_player(socket.assigns.mm_match_id, socket.assigns.mm_player_id)
+    socket
   end
 end
